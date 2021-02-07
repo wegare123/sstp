65684068df8fa1c7966ebdea03f277d1d95b18c9
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-02-08 04:24:12 +0700

    Add files via upload

--- autorekonek-sstp.sh
+++ autorekonek-sstp.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #sstp (Wegare)
-host="$(cat /root/akun/sstp.txt | tr '\n' ' '  | awk '{print $1}')" 
-route="$(route | grep $host| head -n1 | awk '{print $1}')" 
 route2="$(route | grep default | grep ppp | head -n1 | awk '{print $8}')" 
 route3="$(lsof -i | grep pppd)" 
-	if [[ -z $route ]]; then
+	if [[ -z $route2 ]]; then
 		   (printf '3\n'; sleep 15; printf '\n') | sstp	
-           exit
-    elif [[ -z $route2 ]]; then
-           (printf '3\n'; sleep 15; printf '\n') | sstp	
            exit
     elif [[ -z $route3 ]]; then
            (printf '3\n'; sleep 15; printf '\n') | sstp	
