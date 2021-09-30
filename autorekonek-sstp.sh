93f354ff7941fb65b68cfec415b306410fd7f4d0
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2021-09-30 09:44:39 +0700

    Add files via upload

--- autorekonek-sstp.sh
+++ autorekonek-sstp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sstp (Wegare)
 route2="$(route | grep default | grep ppp | head -n1 | awk '{print $8}')" 
-route3="$(lsof -i | grep pppd)" 
+route3="$(netstat -plantu | grep pppd)" 
 	if [[ -z $route2 ]]; then
 		   printf '\n' | sstp
            exit
